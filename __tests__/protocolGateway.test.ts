486cb30760eec201ee1e926205e9c7658d585b8f
Michael J Burgess <beitmenotyou+github@gmail.com>
2025-03-31 13:59:20 +0100

    Delete __tests__ directory
    Signed-off-by: Michael J Burgess <beitmenotyou+github@gmail.com>

--- __tests__/protocolGateway.test.ts
+++
@@ -1,8 +0,0 @@
-// __tests__/protocolGateway.test.ts
-
-describe("OmniSocial Protocol Gateway", () => {
-  it("should respond with 200 on root endpoint", async () => {
-    // Example placeholder test
-    expect(200).toBe(200);
-  });
-});
