9c121cfdf9eb4eca19392ec6bf600d5dd34f0633
Michael J Burgess <beitmenotyou+github@gmail.com>
2025-04-01 23:20:36 +0100

    Delete github-upload.sh
    Signed-off-by: Michael J Burgess <beitmenotyou+github@gmail.com>

--- github-upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Uploads key folders to GitHub and commits them
-git init
-git remote add origin https://github.com/YOUR_USERNAME/omnisocial-hub.git
-git checkout -b main
-git add .
-git commit -m "Initial OmniSocial Hub upload"
-git push -u origin main
